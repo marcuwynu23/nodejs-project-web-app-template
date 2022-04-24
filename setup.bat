b428eaea6d499ed93ee545ccece0f2d47181582d
PeculiarProgrammer <marcuwynu23@gmail.com>
2022-04-25 00:13:13 +0700

    Remove Unecessary Files

--- setup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo installing required modules...
-echo ====================================================
-npm i nodemon express nunjucks express-session mongoose   
-echo ====================================================
-echo done.
